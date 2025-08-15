9904f05e5f76c4e65418cdd2fa38da906de996a5
K.SRI DATTA SIDHARDHA <sridatta.k99@gmail.com>
2025-08-15 21:40:17 +0530

    Update input/output.java

---
+++ input/output.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args)
+  {
+      System.out.println("hello, enter your age");
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      System.out.println(n);
+    }
+
+}
